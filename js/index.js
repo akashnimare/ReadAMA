e295a792ee40bfbd1a4adb7f61689f31c1a81564
akashnimare <svnitakash@gmail.com>
2016-05-19 19:15:27 +0530

    :zap: Error handling fixed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
       $scope.getGitInfo = function () {
          $scope.userNotFound = false;
          $scope.loaded = false;
-         $http.get("https://api.github.com/repos/"+ $scope.username + "/ama/issues?state=all&per_page=300")
+         $http.get("https://api.github.com/repos/" + $scope.username + "/ama/issues?state=all&per_page=300")
                .success(function (data) {
                   $scope.user = data;
                   $scope.loaded = true;
@@ -10,20 +10,10 @@
                .error(function () {
                   $scope.userNotFound = true;
                });
-    }
-
-    $scope.myFunction = function (href,data,event){
+        $scope.myFunction = function (href){
     $http.get(href).success(function(data1) {
     $scope.user1 = data1;
-    $scope.href = href;
-    $scope.select= function(item) {
-         $scope.selected = item; 
-  };
-    $scope.isActive = function(item) {
-        return $scope.selected === item;
- };
-    });
-     }
+});
+    }
+      }
    }
-
-
