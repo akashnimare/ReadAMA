dcd67a8bc70732f45289921b05e2b13337ebdcd0
akashnimare <svnitakash@gmail.com>
2016-07-18 13:48:04 +0530

    Adding pages

---
+++ js/index.js
@@ -0,0 +1,43 @@
+var app = angular.module('AMA', ['ngRoute','hc.marked']);
+
+app.controller('gitCtrl', function($scope, $http, marked) {
+  $scope.oneAtATime = true;
+    $scope.main = 
+            {
+               page: 1
+            };
+            $scope.getGitInfo = function () 
+            {
+            $scope.userNotFound = false;
+            $scope.loaded = false;
+            $scope.nouser = false;
+            $http.get("https://api.github.com/repos/" + $scope.username + "/ama/issues?state=all&page="+ $scope.main.page + "&per_page=100")
+                 .success(function (data) 
+                 {
+                  $scope.user = data;
+                  $scope.loaded = true;
+                 })
+                  .error(function () 
+                  {
+                    $scope.userNotFound = true;
+                  });
+            $scope.UserComment = function (event)
+            {
+            $http.get(event.target.id).success(function(data1) 
+            {
+            $scope.user1 = data1;
+            });
+            }
+            }
+            $scope.nextPage = function() 
+            {
+                  $scope.main.page++;
+                  $scope.getGitInfo();
+            };
+            $scope.prePage = function() {          
+                  $scope.main.page--;
+                  $scope.getGitInfo();
+            };
+
+});
+
