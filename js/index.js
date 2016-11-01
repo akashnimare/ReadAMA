6f7fd1e33cbed9426f6dc2a7fa54a31a1286d0a1
davidepastore <pasdavide@gmail.com>
2016-11-01 23:51:17 +0100

    Closed #6

--- js/index.js
+++ js/index.js
@@ -2,43 +2,43 @@ var app = angular.module('AMA', ['ngRoute','hc.marked']);
 
 app.controller('gitCtrl', function($scope, $http, $location, marked) {
   $scope.oneAtATime = true;
-    $scope.main = 
+    $scope.main =
             {
                page: 1
             };
-            $scope.getGitInfo = function () 
+            $scope.getGitInfo = function ()
             {
             $location.path($scope.username);
             $scope.userNotFound = false;
             $scope.loaded = false;
             $scope.nouser = false;
             $http.get("https://api.github.com/repos/" + $scope.username + "/ama/issues?state=closed&page="+ $scope.main.page + "&per_page=100")
-                 .success(function (data) 
+                 .success(function (data)
                  {
                   $scope.user = data;
                   $scope.loaded = true;
                  })
-                  .error(function () 
+                  .error(function ()
                   {
                     $scope.userNotFound = true;
+                    $scope.usernameNotFound = $scope.username;
                   });
             $scope.UserComment = function (event)
             {
-            $http.get(event.target.id).success(function(data1) 
+            $http.get(event.target.id).success(function(data1)
             {
             $scope.user1 = data1;
             });
             }
             }
-            $scope.nextPage = function() 
+            $scope.nextPage = function()
             {
                   $scope.main.page++;
                   $scope.getGitInfo();
             };
-            $scope.prePage = function() {          
+            $scope.prePage = function() {
                   $scope.main.page--;
                   $scope.getGitInfo();
             };
 
 });
-
